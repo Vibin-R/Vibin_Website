11fcb180f25d121c9821f123f8ce0b6583623b90
VibinRaj-0407 <108596832+VibinRaj-0407@users.noreply.github.com>
2023-06-13 12:36:43 +0000

    Improved some features like contact and some styling

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+PROJECTS = [{
+ 'name' : 'Web Phishing Detection',
+  'summary' : 'Detect the ilegal scam websites',
+    'link' : 'projlink'},
+            {
+ 'name' : 'Solar Tracking System',
+  'summary' : 'Detect the movement of the sun',
+    'link' : 'projlink'},
+            {
+ 'name' : 'RFID Tracker',
+  'summary' : 'Security system that based on RFID',
+    'link' : 'projlink'}]
+
 @app.route('/')
-def hello_world():
-  return "hello"
+def hello():
+  return render_template('home.html',project=PROJECTS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
